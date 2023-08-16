2740de352776e9e941a9797c11fa408adb1c10ae
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-16 23:12:37 +0200

    my success project

---
+++ src/main/java/com/sip/ams/Amsdata16Application.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amsdata16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amsdata16Application.class, args);
+	}
+
+}
